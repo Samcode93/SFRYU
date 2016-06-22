f7cc239cd5a1a987e1310746bfdc6f838c81cf4f
samcode93 <sundeepk.samra@gmail.com>
2016-06-22 14:09:45 -0400

    .

---
+++ js/app.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+    
+$(document).keydown(function(keypressed) {
+
+});
+
+
+});
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
+
+    //Keydown
+    $(document).keydown(function(keypressed){
+        if (keypressed.keyCode === 88) {
+            $(".ryu-ready").hide();
+            $(".ryu-still").hide();
+            $(".ryu-throwing").hide();
+            $(".ryu-cool").show();
+            $(".main").addClass('main-x');
+        }   
+    });
+    
